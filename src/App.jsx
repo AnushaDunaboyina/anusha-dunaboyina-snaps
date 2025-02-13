d6b0bb02ac291f149a35e4569e39a956873c1f7c
Anusha Dunaboyina <anusha.dunaboyina@gmail.com>
2025-02-12 23:25:28 -0500

    Add functionality to filters button and filter tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import FilterDrawer from './components/FilterDrawer/FilterDrawer';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import FilterDrawer from "./components/FilterDrawer/FilterDrawer";
 
-import tagsArray from '../src/data/tags.json';
+import tagsArray from "../src/data/tags.json";
 
 function App() {
-
   // useState for Filters button
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const toggleFilter = () => {
     setIsFilterOpen(!isFilterOpen);
-  }
+  };
 
   //  useState for Tags button
-  const [isTagOn, setIsTagOn] = useState(false);
-  const toggleTag = () => {
-    setIsOn(!isTagOn);
-  }
-
+  const [activeTag, setActiveTag] = useState("");
+  const toggleTag = (tag) => {
+    setActiveTag((previousTag) => (previousTag === tag ? "" : tag));
+  };
 
   return (
     <>
-      <Header handleToggleFilters={toggleFilter} CheckIsFilterOpen={isFilterOpen} />
-      <FilterDrawer tags={tagsArray} handleToggleTag={toggleTag} checkIsTagOn={isTagOn}/>
+      <Header
+        handleToggleFilters={toggleFilter}
+        CheckIsFilterOpen={isFilterOpen}
+      />
+      {isFilterOpen && (
+        <FilterDrawer
+          tags={tagsArray}
+          handleToggleTag={toggleTag}
+          checkActiveTag={activeTag}
+        />
+      )}
+      <
       <Footer />
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/FilterDrawer/FilterDrawer.jsx
+++ src/components/FilterDrawer/FilterDrawer.jsx
@@ -1,13 +1,19 @@
 import "./FilterDrawer.scss";
 
-function FilterDrawer({ tags, handleToggleTag, checkIsTagOn }) {
+function FilterDrawer({ tags, handleToggleTag, checkActiveTag }) {
+    console.log("FilterDrawer is open");
   return (
     <div className="filters__container">
       <h3 className="filters__title">Filters</h3>
       <div className="filters__tags-section">
         {tags.map((tag) => (
-          <button key={tag} className={`filters__tag ${checkIsTagOn ? 'filters__tag--active' : ''}`}
-          onClick={handleToggleTag}>
+          <button
+            key={tag}
+            className={`filters__tag ${
+              checkActiveTag === tag ? "filters__tag--active" : ""
+            }`}
+            onClick={() => handleToggleTag(tag)}
+          >
             {tag}
           </button>
         ))}
