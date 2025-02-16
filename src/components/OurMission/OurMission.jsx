632708908e56909cad7997528f5e7c030e6fccbb
Anusha Dunaboyina <anusha.dunaboyina@gmail.com>
2025-02-15 19:30:33 -0500

    Add tablet and desktop view styling to header and main section

--- src/components/OurMission/OurMission.jsx
+++ src/components/OurMission/OurMission.jsx
@@ -1,14 +1,18 @@
-import './OurMission.scss';
+import "./OurMission.scss";
 
 function OurMission() {
-    return (
-        <div className="Mission-section">
-            <h2 className="Mission-section__title">Our mission:</h2>
-            <p className="Mission-section__paragraph">
-                Provide photographers a space to share photos of the neigborhoods they cherish, <span className="Mission-section__paragraph--italic">expressed in their unique style.</span>
-            </p>
-        </div>
-    )
+  return (
+    <div className="Mission-section">
+      <h2 className="Mission-section__title">Our mission:</h2>
+      <p className="Mission-section__paragraph">
+        Provide photographers a space to share photos of the neigborhoods they
+        cherish,{" "}
+        <span className="Mission-section__paragraph--italic">
+          expressed in their unique style.
+        </span>
+      </p>
+    </div>
+  );
 }
 
 export default OurMission;
