9630e6f50b1987cdb207009fd6e2e0e959c2fabc
Anusha Dunaboyina <anusha.dunaboyina@gmail.com>
2025-02-17 13:43:20 -0500

    Modifying styling

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5010, // Your server port in the 50xx range
-    hmr: {
-      port: 5010, // WebSocket port for Hot Module Replacement (HMR)
-    },
-  },
-})
+});
