efc8aabc5754dea3f6133f1f2669989deaf84052
Anusha Dunaboyina <anusha.dunaboyina@gmail.com>
2025-02-19 18:00:34 -0500

    Replace static tags and photos data with API fetch using useEffect and API key

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import FilterDrawer from "../../components/FilterDrawer/FilterDrawer";
 import OurMission from "../../components/OurMission/OurMission";
 import PhotoCards from "../../components/PhotoCards/PhotoCards";
 
-import tagsArray from "../../data/tags.json";
-import photos from "../../data/photos.json";
+const API_KEY = "71b01ef3-c48c-463a-9ddb-4f2e5372cb75";
+const API_URL = "https://unit-3-project-c5faaab51857.herokuapp.com";
 
 function Home({ isFilterOpen, activeTag, setActiveTag }) {
+  const [tags, setTags] = useState([]);
+  const [photos, setPhotos] = useState([]);
+
+  // Fetch tags and Photos from the API
+  useEffect(() => {
+    const fetchTags = async () => {
+      const response = await axios.get(`${API_URL}/tags?api_key=${API_KEY}`);
+      setTags(response.data);
+    };
+    fetchTags();
+  }, []);
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      const response = await axios.get(`${API_URL}/photos?api_key=${API_KEY}`);
+      setPhotos(response.data);
+    };
+    fetchPhotos();
+  }, []);
+
   const toggleTag = (tag) => {
     setActiveTag((previousTag) => (previousTag === tag ? "" : tag));
   };
@@ -20,7 +43,7 @@ function Home({ isFilterOpen, activeTag, setActiveTag }) {
         {isFilterOpen && (
           <div className="main-right">
             <FilterDrawer
-              tags={tagsArray}
+              tags={tags}
               handleToggleTag={toggleTag}
               checkActiveTag={activeTag}
             />
